55675e8d1aec5bb818a675b636e6aec324fd5119
Adlet Taigulov <adlettaigulov@gmail.com>
2025-11-09 16:57:00 +0500

    Set up light and dark mode, Shadcn, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -25,9 +27,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${inter.variable} antialiased`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
-  function test() {
-    const a = 1;
-    console.log(a);
-  }
-  test();
-
-  return <h1 className="text-3xl text-light-500">Hello World!</h1>;
+  return (
+    <>
+      <div>Home Page</div>
+    </>
+  );
 }
